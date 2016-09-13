e7473effd43e8b33f701702b5d47b65a5e187725
devming <cmk330@gmail.com>
2016-09-14 05:45:08 +0900

    기본 파일 추가 및 세팅

---
+++ Slim_professor/ViewModel/ViewModelPageSubjectStatistic.cs
@@ -0,0 +1,34 @@
+﻿using MVVMBase.Command;
+using MVVMBase.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Slim_professor.Model;
+using Slim_professor.View;
+
+namespace Slim_professor.ViewModel
+{
+    class ViewModelPageSubjectStatistic
+    {
+        /*
+         * TODO 수업통계
+         * ==날짜별로==
+         * 빨간불 횟수
+         * 질문 횟수
+         * 과제
+         * 진도
+         * 특이사항
+         * ============
+         * 콤보박스로 날짜선택하고 위의 내용들 뜨도록
+         * 
+         * 테이블하나 만들어서 저장해야할듯
+         */
+        public ViewModelPageSubjectStatistic(PageSubjectStatistic pWindow)
+        {
+
+        }
+    }
+}
